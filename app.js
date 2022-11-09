2bd44e98cde5a4e4f553d45bc524f5a0221956c3
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2022-11-09 21:42:06 +0300

    Connecting the template engine

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs',require('ejs-locals'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
