dad3ff094fcd0f9f84864971281d3da006eef5f6
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2022-11-09 20:14:24 +0300

    Creating object pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Страница ценникодержателя */
+router.get('/price_tag_holder', function(req, res, next) {
+  res.send("<h1>Страница ценникодержателя</h1>")
 });
+
+/* Страница менюхолдера */
+router.get('/menuholder', function(req, res, next) {
+  res.send("<h1>Страница менюхолдера</h1>")
+});
+
+/* Страница салфетницы */
+router.get('/napkin_holder', function(req, res, next) {
+  res.send("<h1>Страница салфетницы</h1>")
+});
+
+/* Страница  таблички*/
+router.get('/tablet', function(req, res, next) {
+  res.send("<h1>Страница таблички</h1>")
+});
+
 
 module.exports = router;
