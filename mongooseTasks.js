637e91c13fd2e7cf9d14eb54f1950c07c601bbd1
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2022-12-03 04:22:31 +0300

    git commit -m

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Prod = mongoose.model('Prod', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.complete = function(){
+    console.log(this.get("name") + " готов!")
+}
+var Prod = mongoose.model('Prod', schema)
 
 var item = new Prod({ name: 'Значок' })
 item.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Готово')
-    }
+    item.complete()
 })
+
