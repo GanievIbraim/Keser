d8d3cf66db59a085dacd5987a91c6c34b3c29e06
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2022-12-03 04:47:35 +0300

    Creating a model

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/test_1')
+var Product = require("./models/product").Product
 
-var schema = mongoose.Schema({ name: String })
-
-schema.methods.complete = function(){
-    console.log(this.get("name") + " готов!")
-}
-var Prod = mongoose.model('Prod', schema)
-
-
-var item = new Prod({ name: 'Значок' })
-item.save(function (err) {
-    item.complete()
+var product = new Product({
+	title: "Значок",
+	nick: "pin"
 })
+
+console.log(product)
+
+product.save(function(err, product, affected){
+    console.log(product.title)
+})
+
 
