67ed4dec730caeb1a0c5404907b35691b04aa2a8
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2022-12-03 03:38:57 +0300

    Creating a data module

--- createDB.js
+++ createDB.js
@@ -1,15 +1,16 @@
 var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
 
 const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
 	try {
 		await client.connect();
-		var database = client.db("keser");
+		var database = client.db("Keser");
 		database.dropDatabase()
-		database = client.db("keser");
-		const products = database.collection("products");
-		const result = await products.insertOne({name:"Брелок"});
+		database = client.db("Keser");
+		const Products = database.collection("Products");
+		const result = await Products.insertOne({name:"Брелок"});
 		console.log(`${result} documents were inserted`);
 	} finally {
 		await client.close();
