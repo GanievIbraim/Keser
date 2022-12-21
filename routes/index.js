68fbed512b8c2cfa87f42e46f514a49b456a296e
GanievIbraim <ganiev.i.i.21@gmail.com>
2022-12-21 21:02:55 +0300

    «Errors»

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
+/*
 var express = require('express');
 var router = express.Router();
 var Product = require("../models/product").Product
 var User = require("./../models/user").User
+*/
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express', counter: req.session.counter });
 });
+*/
 
-/* GET login/registration page. */
+/* GET login/registration page. 
 router.get('/logreg', function(req, res, next) {
-    res.render('logreg',{title: 'Вход'});
+    res.render('logreg',{title: 'Вход', error:null});
 });
 
 router.post('/logreg', function(req, res, next) {
@@ -23,7 +26,7 @@ router.post('/logreg', function(req, res, next) {
                 req.session.user = user._id
                 res.redirect('/')
             } else {
-                      res.render('logreg', {title: 'Вход'})
+                res.render('logreg',{error:"Пароль не верный"});
             }
        } else {
        var user = new User({username:username,password:password})
@@ -36,5 +39,45 @@ router.post('/logreg', function(req, res, next) {
     })
 });
 
+
+module.exports = router;
+*/
+
+var express = require('express');
+var router = express.Router();
+var Product = require("../models/product").Product
+var User = require("./../models/user").User
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express', counter: req.session.counter });
+});
+
+router.get('/logreg', function(req, res, next) {
+    res.render('logreg',{title: 'Вход', error:null});
+});
+
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+    User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+    if(!password){
+    req.session.user = user._id
+    res.redirect('/')
+    } else {
+    res.render('logreg', {title: 'Вход', error:"Пароль не верный"})
+    }
+    } else {
+    var user = new User({username:username,password:password})
+    user.save(function(err,user){
+    if(err) return next(err)
+    req.session.user = user._id
+    res.redirect('/')
+    })
+    }
+    })
+    });
 
 module.exports = router;
